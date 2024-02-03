3ac26ef5a55e0988c926b429e5831a72cdef59d1
rushibodhe <bodhe.rushikesh1@gmail.com>
2024-02-03 18:13:12 +0530

    added different assertions tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -16,71 +16,77 @@ it('should render same text passed into title prop', () => {
 });
 
 
-// b. using getByRole method:
-// Note: We are using getByRole method due to which it expects only a single heading element to be present in the screen if there are mroe than 1 headings then this test will fail.
+// // b. using getByRole method:
+// // Note: We are using getByRole method due to which it expects only a single heading element to be present in the screen if there are mroe than 1 headings then this test will fail.
+// // it('should render a heading element', () => {
+// //     render(<Header title="My Header" />);  // rendering the component with prop My Header
+// //     const headingElement = screen.getByRole('heading');  // checking the rendered element has a role of heading (ie h1)
+// //     expect(headingElement).toBeInTheDocument();
+// // });
+
+
+// // c. using getByRole method with {name: "My Header"} object:
 // it('should render a heading element', () => {
 //     render(<Header title="My Header" />);  // rendering the component with prop My Header
-//     const headingElement = screen.getByRole('heading');  // checking the rendered element has a role of heading (ie h1)
+//     const headingElement = screen.getByRole('heading', { name: "My Header" });  // checking the rendered element has a role of heading (ie h1) and the name as My Header
 //     expect(headingElement).toBeInTheDocument();
 // });
 
 
-// c. using getByRole method with {name: "My Header"} object:
-it('should render a heading element', () => {
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = screen.getByRole('heading', { name: "My Header" });  // checking the rendered element has a role of heading (ie h1) and the name as My Header
-    expect(headingElement).toBeInTheDocument();
-});
+
+// // 2. Using Priority 2 ie semantic queries:
+// // Using getByTitle:
+// it('should render a heading element which has text header', () => {
+//     render(<Header title="My Header" />);  // rendering the component with prop My Header
+//     const headingElement = screen.getByTitle('header');  // checking the rendered element has a role of heading (ie h1) and the name as My Header
+//     expect(headingElement).toBeInTheDocument();
+// });
 
 
 
-// 2. Using Priority 2 ie semantic queries:
-// Using getByTitle: 
-it('should render a heading element which has text header', () => {
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = screen.getByTitle('header');  // checking the rendered element has a role of heading (ie h1) and the name as My Header
-    expect(headingElement).toBeInTheDocument();
-});
+// // 3. Using Priority 3 ie Test ID:
+// it('should render a heading element which has test id header-1', () => {
+//     render(<Header title="My Header" />);  // rendering the component with prop My Header
+//     const headingElement = screen.getByTestId('header-1');  // checking the rendered element has a role of heading (ie h1) and the name as My Header
+//     expect(headingElement).toBeInTheDocument();
+// });
 
 
 
-// 3. Using Priority 3 ie Test ID:
-it('should render a heading element which has test id header-1', () => {
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = screen.getByTestId('header-1');  // checking the rendered element has a role of heading (ie h1) and the name as My Header
-    expect(headingElement).toBeInTheDocument();
-});
+// // Using findBy method: (async tests)
+// it('should render same text passed into title prop async', async () => { // note we have added async here
+//     render(<Header title="My Header" />);  // rendering the component with prop My Header
+//     const headingElement = await screen.findByText(/my header/i);  //Note we have added await here for finding the data from screen using getByText method
+//     expect(headingElement).toBeInTheDocument();
+// });
+
+
+// // Using queryBy method:
+// // Why we need queryBy method:
+// it('should render same text passed into title prop using getBy', () => {
+//     render(<Header title="My Header" />);  // rendering the component with prop My Header
+//     const headingElement = screen.getByText(/dogs/i);  // as "dogs" text is not present in the screen hence this will fail here itself but in next line we have written that fail this test if the "dogs" text is not present in the screen but because of test does not even goes to the next line and it fails at the top itself hence we need queryBy method
+//     expect(headingElement).not.toBeInTheDocument(); //
+// });
 
 
 
-// Using findBy method: (async tests)
-it('should render same text passed into title prop async', async () => { // note we have added async here
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = await screen.findByText(/my header/i);  //Note we have added await here for finding the data from screen using getByText method
-    expect(headingElement).toBeInTheDocument();
-});
+// it('should render same text passed into title prop using queryBy', () => {
+//     render(<Header title="My Header" />);  // rendering the component with prop My Header
+//     const headingElement = screen.queryByText(/dogs/i);  // finding the data from screen using queryByText method
+//     expect(headingElement).not.toBeInTheDocument(); // Note we have added .not so that to pass this test case we are not expecting "dogs" text to be present in screen
+// });
 
 
-// Using queryBy method:
-// Why we need queryBy method:
-it('should render same text passed into title prop using getBy', () => {
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = screen.getByText(/dogs/i);  // as "dogs" text is not present in the screen hence this will fail here itself but in next line we have written that fail this test if the "dogs" text is not present in the screen but because of test does not even goes to the next line and it fails at the top itself hence we need queryBy method
-    expect(headingElement).not.toBeInTheDocument(); // 
-});
+// // getAllByRole:
+// it('should get all elements which have heading role', () => {
+//     render(<Header title="My Header" />);
+//     const headingElements = screen.getAllByRole("heading"); // taking all elements which has "heading" role
+//     expect(headingElements.length).toBe(2);  // note here we have use .length along with .toBe(2) assertion
+// });
 
 
 
-it('should render same text passed into title prop using queryBy', () => {
-    render(<Header title="My Header" />);  // rendering the component with prop My Header
-    const headingElement = screen.queryByText(/dogs/i);  // finding the data from screen using queryByText method
-    expect(headingElement).not.toBeInTheDocument(); // Note we have added .not so that to pass this test case we are not expecting "dogs" text to be present in screen
-});
 
 
-// getAllByRole:
-it('should get all elements which have heading role', () => {
-    render(<Header title="My Header" />);
-    const headingElements = screen.getAllByRole("heading"); // taking all elements which has "heading" role
-    expect(headingElements.length).toBe(2);  // note here we have use .length along with .toBe(2) assertion
-});
+
---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,104 @@
+import { render, screen } from '@testing-library/react';
+import TodoFooter from '../TodoFooter';
+import { BrowserRouter } from 'react-router-dom';
+
+
+// In this test case our objective is to find the how many number of tasks it shows on screen?
+
+// Problems with router while testing and how to solve them?
+// Note: This test case will fail because we have used router in the TodoFooter component and currently we are testing this component on complete isolation and we cannot test our component in isolation if it has a router hence to avoid this error check the corrected code:
+
+// Incorrect code for router
+// it('should render the correct amount of incomplete tasks', () => {
+//     render(<TodoFooter numberOfIncompleteTasks={5} />);
+//     const paragraphElement = screen.getByText(/5 tasks left/i);
+//     expect(paragraphElement).toBeInTheDocument();
+// });
+
+
+
+// Corrected code for router:
+// Creating mock TodoFooter:
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+
+    return (
+        <BrowserRouter>
+            <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+        </BrowserRouter>
+    )
+}
+
+
+// Then passing that MockTodoFooter 
+it('should render the correct amount of incomplete tasks', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+});
+
+
+
+// Different types of assertions:
+// toBeInTheDocument():
+it('should render "task" singular when the number of incomplete tasks in one', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+});
+
+
+// toBeVisible(): failed testcase deliberately
+// Note: difference between toBeVisible and toBeInTheDocument is that if there is opacity 0 style added to the element then it will not be visible to the user hence in this case we can use this property
+// This will fail because we have added style = {{"opcaity": 0}}
+it('should render "task" singular when the number of incomplete tasks in one with toBeVisible assertion', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeVisible();  // This assertion method allows you to check if an element is currently visible to the user
+});
+
+
+// toContainHTML():
+it('should render "task" singular when the number of incomplete tasks in one with toContainHTML assertion', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toContainHTML("p");
+});
+
+
+// toHaveTextContent():
+// Note: Its used when you couldnt get the text from screen because you used a getByRole attribute:
+it('should render "task" singular when the number of incomplete tasks in one with toHaveTextContent assertion', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement).toHaveTextContent("1 task left");
+});
+
+
+
+// .not.anyassertion_method():
+// Note: Its used when you want the opposite of the provided assertion method:
+it('should render "task" singular when the number of incomplete tasks in one with .not assertion', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement).not.toBeVisible()
+});
+
+
+
+// You can also use element's properties while assertion:
+it('should render "task" singular when the number of incomplete tasks in one with textContent added in assertion', () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByTestId("para");
+    expect(paragraphElement.textContent).toBe("1 task left")  // using .textContent property while assertion 
+});
+
+
+// Note there can be multiple assertions also but its not recommended because it can fail the whole test and we need to check which assertion has actually failed.
+// Example:
+// it('should render "task" singular when the number of incomplete tasks in one with textContent added in assertion', () => {
+//     render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//     const paragraphElement = screen.getByTestId("para");
+//     expect(paragraphElement.textContent).toBe("1 task left")  // using .textContent property while assertion
+//     expect(paragraphElement.textContent).toBe("1 task left")  // using .textContent property while assertion
+//     expect(paragraphElement.textContent).toBe("1 task left")  // using .textContent property while assertion
+// });
